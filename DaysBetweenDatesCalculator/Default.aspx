260559377db9143c5e6196608657772312c99188
zackfactual <zackfactual@gmail.com>
2018-01-09 09:06:48 -0800

    initial commit

---
+++ DaysBetweenDatesCalculator/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="DaysBetweenDatesCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+		<h4>Days Between Dates Calculator</h4>
+		Select a date:<br />
+		<asp:Calendar ID="firstCalendar" runat="server"></asp:Calendar>
+		<br />
+		Select a second date:<asp:Calendar ID="secondCalendar" runat="server">
+		</asp:Calendar>
+		<br />
+		<asp:Button ID="calculateButton" runat="server" OnClick="calculateButton_Click" Text="Calculate" />
+		<br />
+		<br />
+		<asp:Label ID="resultLabel" runat="server"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ DaysBetweenDatesCalculator/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DaysBetweenDatesCalculator
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		protected void calculateButton_Click(object sender, EventArgs e)
+		{
+			if (firstCalendar.SelectedDate == default(DateTime) || secondCalendar.SelectedDate == default(DateTime))
+				resultLabel.Text = "Please select two valid dates.";
+			else if (firstCalendar.SelectedDate > secondCalendar.SelectedDate)
+			{
+				TimeSpan daysElapsed = firstCalendar.SelectedDate - secondCalendar.SelectedDate;
+				resultLabel.Text = String.Format("There are {0} days between {1} and {2}",
+					daysElapsed.TotalDays.ToString(),
+					secondCalendar.SelectedDate.ToLongDateString(),
+					firstCalendar.SelectedDate.ToLongDateString());
+			}
+			else
+			{
+				TimeSpan daysElapsed = secondCalendar.SelectedDate - firstCalendar.SelectedDate;
+				resultLabel.Text = String.Format("There are {0} days between {1} and {2}",
+					daysElapsed.TotalDays.ToString(),
+					firstCalendar.SelectedDate.ToLongDateString(),
+					secondCalendar.SelectedDate.ToLongDateString());
+			}
+		}
+	}
+}
